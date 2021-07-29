51593b34f735d5cd20389a1ad02d11f82c6b981f
NookieNoo <biv0208@yandex.ru>
2021-07-29 21:43:24 +0300

    init

---
+++ exersizes/ex34SendDataToPrevDisplay/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/first_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Returned Pass Data',
+    home: FirstScreen(),
+  ));
+}
